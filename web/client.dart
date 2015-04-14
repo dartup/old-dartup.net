ab77c83f89d124ccbf5cee70a09562523e9742a6
Ole Martin Gjersvik <olem@olem.org>
2015-04-14 15:03:30 +0200

    Removed readmore is not needed any more

--- web/client.dart
+++ web/client.dart
@@ -6,20 +6,7 @@ import "client_lib.dart";
 import "wrappers/location_wrapper.dart";
 
 main(){
-  readmore();
-  
   var server = new Server();
   var auth = new Auth(new LocationWrapper(), server, window);
   //querySelector("#signin_button").onClick.listen((_)=> auth.login());
-}
-
-readmore(){
-  querySelectorAll('.readmore').onClick.listen((MouseEvent e){
-    if(e.button == 0){ 
-      Element aticle = (e.target as Element).parent;
-      aticle.querySelector(".long").classes.toggle('hidden');
-      e.preventDefault();
-      e.stopPropagation();
-    }
-  });
 }
